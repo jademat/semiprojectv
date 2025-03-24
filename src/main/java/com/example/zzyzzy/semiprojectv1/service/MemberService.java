6b8bedb05ef81014ec419debf594822b3530e794
jademat <01088049128a@gmail.com>
2025-03-24 10:18:20 +0900

    CustomUserDetailsService 구현

--- src/main/java/com/example/zzyzzy/semiprojectv1/service/MemberService.java
+++ src/main/java/com/example/zzyzzy/semiprojectv1/service/MemberService.java
@@ -2,8 +2,12 @@ package com.example.zzyzzy.semiprojectv1.service;
 
 import com.example.zzyzzy.semiprojectv1.domain.Member;
 import com.example.zzyzzy.semiprojectv1.domain.MemberDTO;
+import com.example.zzyzzy.semiprojectv1.domain.User;
 import com.example.zzyzzy.semiprojectv1.repository.MemberRepository;
+import com.example.zzyzzy.semiprojectv1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberMapper;
+    private final UserRepository userRepository;
 
     public boolean newMember(MemberDTO member) {
         
@@ -28,15 +33,22 @@ public class MemberService {
         return result == 1;  // 회원정보가 테이블 저장되었는지 여부에 따라
                              // true/false 반환
     }
+    // 스프링 시큐리티가 자동으로 처리 - 생략
+//    public Member loginMember(MemberDTO member) {
+//        Member findMember = memberMapper.findByUserid(member.getUserid());
+//
+//        if (findMember == null || !findMember.getPasswd().equals(member.getPasswd())) {
+//            throw new IllegalStateException("아이디나 비밀번호가 일치하지 않습니다!!");
+//        }
+//
+//        return findMember;
+//    }
 
-    public Member loginMember(MemberDTO member) {
-        Member findMember = memberMapper.findByUserid(member.getUserid());
+    public User findbyUserid(UserDetails userDetails) {
+        User findUser = userRepository.findByUserid(userDetails.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("사용자가 존재하지 않습니다."));
 
-        if (findMember == null || !findMember.getPasswd().equals(member.getPasswd())) {
-            throw new IllegalStateException("아이디나 비밀번호가 일치하지 않습니다!!");
-        }
-
-        return findMember;
+        return findUser;
     }
 
 }
